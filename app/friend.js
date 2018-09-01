65bbd20051de899367d3da10d49afe3457307fe3
emme8888 <emme8888@hotmail.com>
2018-08-31 23:40:35 -0500

    homework

---
+++ app/friend.js
@@ -0,0 +1,18 @@
+var friendList = [
+    {
+      name: 'Emme Hasanovic',
+      
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
